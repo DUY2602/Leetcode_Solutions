1d7f46dae28188252f14b8b99815ec9fd3d6c8a1
duydo <duydo7779@gmail.com>
2025-08-30 19:47:47 +0700

    Create 36. Valid Sudoku.py

---
+++ 36. Valid Sudoku.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                char = board[i][j]
+                if char != '.':
+
+                    box_index = (i // 3) * 3 + (j // 3)
+
+                    if char in rows[i] or char in cols[j] or char in boxes[box_index]:
+                        return False
+
+                    rows[i].add(char)
+                    cols[j].add(char)
+                    boxes[box_index].add(char)
+
+        return True
